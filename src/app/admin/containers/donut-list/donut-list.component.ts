c8380277ca19ec491d1d94a89c63be3a890522de
hnv1212 <h19982312@gmail.com>
2023-12-29 22:43:56 +0700

    refactor

--- src/app/admin/containers/donut-list/donut-list.component.ts
+++ src/app/admin/containers/donut-list/donut-list.component.ts
@@ -4,7 +4,44 @@ import { Donut } from '../../models/donut.model';
 @Component({
   selector: 'donut-list',
   templateUrl: './donut-list.component.html',
-  styleUrls: ['./donut-list.component.css'],
+  styles: [
+    `
+      .donut-card {
+        display: flex;
+        align-items: center;
+        background: #f7f7f7;
+        border-radius: 5px;
+        margin-bottom: 5px;
+        padding: 5px 15px;
+        transition: transform 0.2s ease-in-out;
+        &:hover {
+          transform: translateY(-3px);
+        }
+        &-promo {
+          border: 2px solid #eee;
+        }
+        &-name {
+          font-size: 16px;
+        }
+        &-label {
+          border: 1px solid #c14583;
+          border-radius: 4px;
+          padding: 0 4px;
+          margin-left: 5px;
+          font-size: 12px;
+          color: #c14583;
+        }
+        &-price {
+          font-size: 14px;
+          color: #c14583;
+        }
+        &-icon {
+          width: 50px;
+          margin-right: 10px;
+        }
+      }
+    `,
+  ],
 })
 export class DonutListComponent implements OnInit {
   donuts!: Donut[];
@@ -12,19 +49,41 @@ export class DonutListComponent implements OnInit {
   ngOnInit(): void {
     this.donuts = [
       {
-        id: '123456',
-        name: 'donut 1',
-        icon: '',
+        id: 'y8z0As',
+        name: 'Just Chocolate',
+        icon: 'just-chocolate',
         price: 119,
-        description: 'djflkfd kdjflkdsj',
+        promo: 'limited',
+        description: 'For the pure chocoholic.',
       },
       {
-        id: 'a1bsu2',
-        name: 'donut 2',
-        icon: '',
-        price: 123,
-        promo: 'limited',
-        description: 'djflkfd',
+        id: '3u98Kl',
+        name: 'Glazed Fudge',
+        icon: 'glazed-fudge',
+        price: 129,
+        promo: 'new',
+        description: 'Sticky perfection.',
+      },
+      {
+        id: 'ae098s',
+        name: 'Caramel Swirl',
+        icon: 'caramel-swirl',
+        price: 129,
+        description: 'Chocolate drizzled with caramel.',
+      },
+      {
+        id: '8amkZ9',
+        name: 'Sour Supreme',
+        icon: 'sour-supreme',
+        price: 139,
+        description: 'For the sour advocate.',
+      },
+      {
+        id: 'l3M0nz',
+        name: 'Zesty Lemon',
+        icon: 'zesty-lemon',
+        price: 129,
+        description: 'Delicious lucious lemon.',
       },
     ];
   }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,38 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styles: [
+    `
+      .app {
+        background: #fff;
+        border-radius: 8px;
+        max-width: 400px;
+        width: 94%;
+        margin: 25px auto;
+        padding: 25px;
+        border: 4px solid #ef9fc7;
+      }
+      .header {
+        display: flex;
+        justify-content: center;
+        margin-bottom: 25px;
+      }
+      .logo {
+        width: 100px;
+        height: 88px;
+      }
+    `,
+  ],
 })
 export class AppComponent {
-  title = 'angular-projects';
+  message!: string;
+  newMessage!: string;
+
+  ngOnInit() {
+    this.message = 'Hello World';
+  }
+
+  handleClick(event: Event) {
+    console.log(event);
+  }
 }
