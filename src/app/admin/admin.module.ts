256db7fd40812fb87785938b28dcd888a164a378
hnv1212 <h19982312@gmail.com>
2023-12-29 23:01:20 +0700

    part33: Create a Form Container and Dumb Component

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DonutListComponent } from './containers/donut-list/donut-list.component';
 import { DonutCardComponent } from './components/donut-card/donut-card.component';
+import { DonutSingleComponent } from './containers/donut-single/donut-single.component';
+import { DonutFormComponent } from './components/donut-form/donut-form.component';
 
 @NgModule({
-  declarations: [DonutListComponent, DonutCardComponent],
+  declarations: [DonutListComponent, DonutCardComponent, DonutSingleComponent, DonutFormComponent],
   imports: [CommonModule],
-  exports: [DonutListComponent],
+  exports: [DonutListComponent, DonutSingleComponent],
 })
 export class AdminModule {}
--- src/app/admin/containers/donut-list/donut-list.component.ts
+++ src/app/admin/containers/donut-list/donut-list.component.ts
@@ -4,44 +4,7 @@ import { Donut } from '../../models/donut.model';
 @Component({
   selector: 'donut-list',
   templateUrl: './donut-list.component.html',
-  styles: [
-    `
-      .donut-card {
-        display: flex;
-        align-items: center;
-        background: #f7f7f7;
-        border-radius: 5px;
-        margin-bottom: 5px;
-        padding: 5px 15px;
-        transition: transform 0.2s ease-in-out;
-        &:hover {
-          transform: translateY(-3px);
-        }
-        &-promo {
-          border: 2px solid #eee;
-        }
-        &-name {
-          font-size: 16px;
-        }
-        &-label {
-          border: 1px solid #c14583;
-          border-radius: 4px;
-          padding: 0 4px;
-          margin-left: 5px;
-          font-size: 12px;
-          color: #c14583;
-        }
-        &-price {
-          font-size: 14px;
-          color: #c14583;
-        }
-        &-icon {
-          width: 50px;
-          margin-right: 10px;
-        }
-      }
-    `,
-  ],
+  styles: [],
 })
 export class DonutListComponent implements OnInit {
   donuts!: Donut[];
