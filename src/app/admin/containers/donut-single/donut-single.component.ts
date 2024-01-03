ab5d99f882f836f69a7df86e6d2c0d434256cb28
hnv1212 <h19982312@gmail.com>
2024-01-03 16:43:22 +0700

    part52: Share State in Components via Services

--- src/app/admin/containers/donut-single/donut-single.component.ts
+++ src/app/admin/containers/donut-single/donut-single.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Donut } from '../../models/donut.model';
+import { DonutService } from '../../services/donut.service';
 
 @Component({
   selector: 'donut-single',
@@ -13,8 +14,13 @@ import { Donut } from '../../models/donut.model';
 export class DonutSingleComponent implements OnInit {
   donut!: Donut;
 
+  constructor(private donutService: DonutService) {}
+
   ngOnInit(): void {
-    this.donut = {};
+    const id = 'y8z0As';
+    this.donut = this.donutService.donuts.find(
+      (donut: Donut) => donut.id === id
+    ) || { name: '', icon: '', price: 0, description: '' };
   }
 
   onCreate(donut: Donut) {}
