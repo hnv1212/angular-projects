537ce85578271dd3d0cf47ed59bf61626d359558
hnv1212 <h19982312@gmail.com>
2024-01-03 21:09:35 +0700

    part68: Root Module Routes with forRoot()

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DonutListComponent } from './admin/containers/donut-list/donut-list.component';
+import { DonutSingleComponent } from './admin/containers/donut-single/donut-single.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: DonutListComponent },
+  {
+    path: 'donut',
+    component: DonutSingleComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
