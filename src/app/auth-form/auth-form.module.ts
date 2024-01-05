c83f8f477f02c8bd16811651c707a43a3aba0775
hnv1212 <h19982312@gmail.com>
2024-01-05 14:09:13 +0700

    part2: Content projection with ng-content

---
+++ src/app/auth-form/auth-form.module.ts
@@ -0,0 +1,16 @@
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { AuthFormComponent } from "./auth-form.component";
+
+@NgModule({
+    declarations: [AuthFormComponent],
+    imports: [
+        CommonModule,
+        FormsModule
+    ],
+    exports: [
+        AuthFormComponent
+    ]
+})
+export class AuthFormModule {}
