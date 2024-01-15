d38c610bcb00c0d875e51b99ea63639ce42430c6
hnv1212 <h19982312@gmail.com>
2024-01-16 01:31:37 +0700

    part28: Creating a custom pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,44 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `
-    <div>
-      <ul>
-        <li *myFor="let item of items; let i = index">
-          {{ i }} Member: {{ item.name | json }}
-        </li>
-        <!-- <ng-template myFor [myForOf]="items" let-item let-i="index">
-          <li>{{ i }} Member: {{ item.name | json }}</li>
-        </ng-template> -->
-      </ul>
-    </div>
-  `,
+  template: ` <div>
+    <div *ngFor="let file of files">
+      <p>{{file.name}}</p>
+      <p>{{file.size | filesize }}</p>
+  </div>
+  </div> `,
 })
-export class AppComponent {
-  items = [
-    {
-      name: 'Mark Hoppus',
-      age: 44,
-      location: 'California',
-    },
-    {
-      name: 'Tom Delonge',
-      age: 41,
-      location: 'California',
-    },
-    {
-      name: 'Travis Barker',
-      age: 41,
-      location: 'California',
-    },
-  ];
-  constructor() {
-    setTimeout(() => {
-      this.items = [
-        ...this.items,
-        { name: 'Matt Skiba', age: 40, location: 'California' },
-      ];
-    }, 2000);
+export class AppComponent implements OnInit {
+  files!: File[];
+
+  ngOnInit() {
+    this.files = [
+      { name: 'logo.svg', size: 2120109, type: 'image/svg' },
+      { name: 'banner.jpg', size: 18029, type: 'image/jpg' },
+      { name: 'background.png', size: 1784562, type: 'image/png' },
+    ];
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { MyForDirective } from './my-for/my-for.directive';
+import { FilesizePipe } from './filesize.pipe';
 
 @NgModule({
-  declarations: [AppComponent, MyForDirective],
+  declarations: [AppComponent, FilesizePipe],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
