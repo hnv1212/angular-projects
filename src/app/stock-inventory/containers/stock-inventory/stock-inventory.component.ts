7baa6d1754abfd086b2e008837e745e3831fb563
hnv1212 <h19982312@gmail.com>
2024-01-16 14:30:04 +0700

    part31: FormControls and FormGroups

--- src/app/stock-inventory/containers/stock-inventory/stock-inventory.component.ts
+++ src/app/stock-inventory/containers/stock-inventory/stock-inventory.component.ts
@@ -1,14 +1,39 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'stock-inventory',
   template: `
-    <p>
-      stock-inventory works!
-    </p>
+    <div class="stock-inventory">
+      <form [formGroup]="form" (ngSubmit)="onSubmit()">
+        <div formGroupName="store">
+          <input type="text" placeholder="Branch ID" formControlName="branch" />
+          <input
+            type="text"
+            placeholder="Manager Code"
+            formControlName="code"
+          />
+        </div>
+
+        <div class="stock-inventory__buttons">
+          <button type="submit" [disabled]="form.invalid">Order stock</button>
+        </div>
+
+        <pre>{{ form.value | json }}</pre>
+      </form>
+    </div>
   `,
-  styleUrls: ['./stock-inventory.component.scss']
+  styleUrls: ['./stock-inventory.component.scss'],
 })
 export class StockInventoryComponent {
+  form = new FormGroup({
+    store: new FormGroup({
+      branch: new FormControl(''),
+      code: new FormControl(''),
+    }),
+  });
 
+  onSubmit() {
+    console.log(this.form.value);
+  }
 }
--- src/app/stock-inventory/stock-inventory.module.ts
+++ src/app/stock-inventory/stock-inventory.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StockInventoryComponent } from './containers/stock-inventory/stock-inventory.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [StockInventoryComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   exports: [StockInventoryComponent],
 })
 export class StockInventoryModule {}
