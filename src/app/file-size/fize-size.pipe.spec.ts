f73f1df31e3c64bc8e27ea386b9fd0e10a29ccab
hnv1212 <h19982312@gmail.com>
2024-01-21 02:02:39 +0700

    part66: Shallow testing Pipes

--- src/app/file-size/fize-size.pipe.spec.ts
+++ src/app/file-size/fize-size.pipe.spec.ts
@@ -1,4 +1,15 @@
+import { Component } from '@angular/core';
 import { FizeSizePipe } from './fize-size.pipe';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting,
+} from '@angular/platform-browser-dynamic/testing';
+
+// TestBed.initTestEnvironment(
+//   BrowserDynamicTestingModule,
+//   platformBrowserDynamicTesting()
+// );
 
 describe('FizeSizePipe', () => {
   const pipe = new FizeSizePipe();
@@ -21,6 +32,48 @@ describe('FizeSizePipe', () => {
     it('should override the extension when supplied', () => {
       expect(pipe.transform(123456789, 'myExt')).toBe('117.74myExt');
       expect(pipe.transform(987654321, 'anotherExt')).toBe('941.90anotherExt');
+    });
+  });
+
+  describe('Shallow FileSizePipe test', () => {
+    @Component({
+      template: ` Size: {{ size | fizeSize : suffix }} `,
+    })
+    class TestComponent {
+      suffix: any;
+      size = 123456789;
+    }
+
+    let component: TestComponent;
+    let fixture: ComponentFixture<TestComponent>;
+    let el: HTMLElement;
+
+    beforeEach(() => {
+      TestBed.configureTestingModule({
+        declarations: [FizeSizePipe, TestComponent],
+      });
+      fixture = TestBed.createComponent(TestComponent);
+      component = fixture.componentInstance;
+      el = fixture.nativeElement;
+    });
+
+    it('should convert bytes to megabytes', () => {
+      fixture.detectChanges();
+      expect(el.textContent).toContain('Size: 117.74MB');
+      component.size = 1029281;
+      fixture.detectChanges();
+      expect(el.textContent).toContain('Size: 0.98MB');
+    });
+
+    it('should use the default extension when not supplied', () => {
+      fixture.detectChanges();
+      expect(el.textContent).toContain('Size: 117.74MB');
+    });
+
+    it('should override the extension when supplied', () => {
+      component.suffix = 'myExt';
+      fixture.detectChanges();
+      expect(el.textContent).toContain('Size: 117.74myExt');
     });
   });
 });
