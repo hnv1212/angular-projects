5862be7e03b1258e0eb3f7e27071b0441c6dc8b9
hnv1212 <h19982312@gmail.com>
2024-01-21 20:18:53 +0700

    part73: Testing an Attribute Directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { FizeSizePipe } from './file-size/fize-size.pipe';
 import { StockCounterComponent } from './stock-inventory/components/stock-counter/stock-counter.component';
 import { StockInventoryComponent } from './stock-inventory/containers/stock-inventory/stock-inventory.component';
 import { StockInventoryModule } from './stock-inventory/stock-inventory.module';
+import { CreditCardDirective } from './credit-card/credit-card.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FizeSizePipe
+    FizeSizePipe,
+    CreditCardDirective
   ],
   imports: [
     BrowserModule,
