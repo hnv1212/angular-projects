9c8fc0ad95897d6741e8edad336d4aea304f6c5c
hnv1212 <h19982312@gmail.com>
2024-01-21 15:16:25 +0700

    part68: Testing Component methods

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FizeSizePipe } from './file-size/fize-size.pipe';
+import { StockCounterComponent } from './stock-inventory/components/stock-counter/stock-counter.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FizeSizePipe
+    FizeSizePipe,
+    StockCounterComponent
   ],
   imports: [
     BrowserModule
