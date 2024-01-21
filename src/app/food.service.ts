fcf907dd59e67f54a3a23f16a158b2371dddaeb8
hnv1212 <h19982312@gmail.com>
2024-01-21 21:49:58 +0700

    part76: Providers and useClass

--- src/app/food.service.ts
+++ src/app/food.service.ts
@@ -5,7 +5,12 @@ import { API_TOKEN } from './token';
 
 @Injectable()
 export class FoodService {
-  constructor(private http: HttpClient, @Inject(API_TOKEN) private api: string) {}
+  constructor(
+    private http: HttpClient,
+    @Inject(API_TOKEN) private api: string
+  ) {
+    console.log('Ultimate Angular');
+  }
 
   getFood(): Observable<any> {
     return this.http.get(this.api);
