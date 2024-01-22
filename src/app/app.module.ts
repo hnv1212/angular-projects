0d8401a0f12e2a25dcf586c856e23c8c16cdc2f6
hnv1212 <h19982312@gmail.com>
2024-01-22 16:50:40 +0700

    part81: State management init setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { DrinkViewerComponent } from './containers/drink-viewer.component';
-import { PizzaViewerComponent } from './containers/pizza-viewer.component';
-import { SideViewerComponent } from './containers/side-viewer.component';
-import { API_TOKEN } from './token';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    DrinkViewerComponent,
-    PizzaViewerComponent,
-    SideViewerComponent,
-  ],
-  imports: [BrowserModule, HttpClientModule],
-  providers: [
-    {
-      provide: API_TOKEN,
-      useValue: 'http://localhost:3000/pizzas',
-    },
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
