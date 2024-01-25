c4bec973d71e78a3a7f1fa0538218dd7d5a87de5
hnv1212 <h19982312@gmail.com>
2024-01-25 15:29:55 +0700

    part99: Async Pipe "as" syntax and routing

--- src/health/meals/meals.module.ts
+++ src/health/meals/meals.module.ts
@@ -4,16 +4,21 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 import { MealsComponent } from './containers/meals/meals.component';
 import { SharedModule } from '../shared/shared.module';
+import { MealComponent } from './containers/meal/meal.component';
 
 export const ROUTES: Routes = [
   {
     path: '',
     component: MealsComponent,
   },
+  {
+    path: 'new',
+    component: MealComponent,
+  },
 ];
 
 @NgModule({
   imports: [CommonModule, ReactiveFormsModule, RouterModule.forChild(ROUTES), SharedModule],
-  declarations: [MealsComponent],
+  declarations: [MealsComponent, MealComponent],
 })
 export class MealsModule {}
