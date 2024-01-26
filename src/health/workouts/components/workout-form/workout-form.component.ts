4005dcdf76198f8e034b6f487d1ab852bc879533
hnv1212 <h19982312@gmail.com>
2024-01-26 20:20:48 +0700

    part107: Reactive Form conditionals

--- src/health/workouts/components/workout-form/workout-form.component.ts
+++ src/health/workouts/components/workout-form/workout-form.component.ts
@@ -22,7 +22,7 @@ import { Workout } from 'src/health/shared/services/workouts.service';
             <h3>Workout name</h3>
             <input
               type="text"
-              placeholder="e.g. English Breakfast"
+              [placeholder]="placeholder"
               formControlName="name"
             />
             <div class="error" *ngIf="required">Workout name is required</div>
@@ -31,6 +31,38 @@ import { Workout } from 'src/health/shared/services/workouts.service';
             <h3>Type</h3>
             <workout-type formControlName="type"></workout-type>
           </label>
+        </div>
+
+        <div class="workout-form__details">
+          <div *ngIf="form.get('type')?.value === 'strength'">
+            <div class="workout-form__fields" formGroupName="strength">
+              <label>
+                <h3>Reps</h3>
+                <input type="number" formControlName="reps" />
+              </label>
+              <label>
+                <h3>Sets</h3>
+                <input type="number" formControlName="sets" />
+              </label>
+              <label>
+                <h3>Weight <span>(kg)</span></h3>
+                <input type="number" formControlName="weight" />
+              </label>
+            </div>
+          </div>
+
+          <div *ngIf="form.get('type')?.value === 'endurance'">
+            <div class="workout-form__fields" formGroupName="endurance">
+              <label>
+                <h3>Distance <span>(km)</span></h3>
+                <input type="number" formControlName="distance" />
+              </label>
+              <label>
+                <h3>Duration <span>(minutes)</span></h3>
+                <input type="number" formControlName="duration" />
+              </label>
+            </div>
+          </div>
         </div>
 
         <div class="workout-form__submit">
@@ -93,13 +125,33 @@ export class WorkoutFormComponent implements OnChanges {
 
   form = this.fb.group({
     name: ['', Validators.required],
-    type: 'strength'
+    type: 'strength',
+    strength: this.fb.group({
+      reps: 0,
+      sets: 0,
+      weight: 0,
+    }),
+    endurance: this.fb.group({
+      distance: 0,
+    }),
   });
 
   constructor(private fb: FormBuilder) {}
 
   ngOnChanges(changes: SimpleChanges): void {
-    throw new Error('Method not implemented.');
+    if (this.workout && this.workout.name) {
+      this.exists = true;
+      const value = this.workout;
+      this.form.patchValue(value);
+    }
+  }
+
+  get placeholder() {
+    return `
+      e.g. ${
+        this.form.get('type')?.value === 'strength' ? 'Benchpress' : 'Treadmill'
+      }
+    `;
   }
 
   get required() {
