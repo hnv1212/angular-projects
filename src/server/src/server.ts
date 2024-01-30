658e42491f95afd225bf2de078d570d8bdef1ef2
hnv1212 <h19982312@gmail.com>
2024-01-30 15:22:36 +0700

    part12: Creating auth module

--- src/server/src/server.ts
+++ src/server/src/server.ts
@@ -2,20 +2,22 @@ import express from 'express';
 import { createServer } from 'http';
 import { Server } from 'socket.io';
 import mongoose from 'mongoose';
+import cors from 'cors';
 
 import * as usersController from './controllers/users';
-import authMiddleware from './middlewares/auth'
+import authMiddleware from './middlewares/auth';
 
 const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer);
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.post('/api/users', usersController.register);
 app.post('/api/users/login', usersController.login);
-app.get('/api/user', authMiddleware, usersController.currentUser)
+app.get('/api/user', authMiddleware, usersController.currentUser);
 
 io.on('connection', () => {
   console.log('connect socket.io');
