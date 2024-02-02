d907092090f6955dff20b11555bfa6410a6ca623
hnv1212 <h19982312@gmail.com>
2024-02-02 19:14:06 +0700

    part43: Get task and columns

--- src/app/board/components/taskModal/taskModal.component.ts
+++ src/app/board/components/taskModal/taskModal.component.ts
@@ -1,17 +1,43 @@
-import { Component, HostBinding } from '@angular/core';
+import { Component, HostBinding, OnDestroy } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { BoardService } from '../../services/board.service';
+import {
+  Observable,
+  Subject,
+  combineLatest,
+  filter,
+  map,
+  takeUntil,
+} from 'rxjs';
+import { TaskInterface } from 'src/app/shared/types/task.interface';
+import { ColumnInterface } from 'src/app/shared/types/column.interface';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'task-modal',
   templateUrl: './taskModal.component.html',
 })
-export class TaskModalComponent {
+export class TaskModalComponent implements OnDestroy {
   @HostBinding('class') classes = 'task-modal';
 
   boardId: string;
   taskId: string;
+  task$: Observable<TaskInterface>;
+  data$: Observable<{
+    task: TaskInterface;
+    columns: ColumnInterface[];
+  }>;
+  columnForm = this.fb.group({
+    columnId: [''],
+  });
+  unsubscribe$ = new Subject<void>();
 
-  constructor(private route: ActivatedRoute, private router: Router) {
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private boardService: BoardService,
+    private fb: FormBuilder
+  ) {
     const taskId = this.route.snapshot.paramMap.get('taskId');
     const boardId = this.route.parent?.snapshot.paramMap.get('boardId');
 
@@ -23,9 +49,34 @@ export class TaskModalComponent {
     }
     this.taskId = taskId;
     this.boardId = boardId;
+    this.task$ = this.boardService.tasks$.pipe(
+      map((tasks) => {
+        return tasks.find((task) => task.id === this.taskId);
+      }),
+      filter(Boolean)
+    );
+    this.data$ = combineLatest([this.task$, this.boardService.columns$]).pipe(
+      map(([task, columns]) => ({ task, columns }))
+    );
+    this.task$.pipe(takeUntil(this.unsubscribe$)).subscribe((task) => {
+      this.columnForm.patchValue({ columnId: task.columnId});
+    });
   }
 
   goToBoard(): void {
     this.router.navigate(['boards', this.boardId]);
+  }
+
+  updateTaskName(taskName: string): void {
+    throw new Error('Method not implemented.');
+  }
+
+  updateTaskDescription(taskDescription: string) {
+    throw new Error('Method not implemented.');
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
   }
 }
