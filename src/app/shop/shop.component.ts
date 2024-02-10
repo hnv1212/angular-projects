ae917c7207d907c64ec037797ee8c785a8a1f33a
hnv1212 <h19982312@gmail.com>
2024-02-10 19:54:07 +0700

    Resolving the Bug!

--- src/app/shop/shop.component.ts
+++ src/app/shop/shop.component.ts
@@ -60,6 +60,7 @@ export class ShopComponent implements OnInit {
 
   onBrandSelected(brandId: number) {
     this.shopParams.brandId = brandId;
+    this.shopParams.pageNumber = 1;
     this.getProducts();
   }
 
@@ -74,12 +75,15 @@ export class ShopComponent implements OnInit {
   }
 
   onPageChanged(event: any) {
-    this.shopParams.pageNumber = event;
-    this.getProducts();
+    if (this.shopParams.pageNumber !== event) {
+      this.shopParams.pageNumber = event;
+      this.getProducts();
+    }
   }
 
   onSearch() {
     this.shopParams.search = this.searchTerm?.nativeElement.value;
+    this.shopParams.pageNumber = 1;
     this.getProducts();
   }
 
