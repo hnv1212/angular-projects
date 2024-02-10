e76c46ed15be0dea41155c168cfd3e4e7d91e9ed
hnv1212 <h19982312@gmail.com>
2024-02-10 21:32:32 +0700

    routing

--- src/app/shop/shop.module.ts
+++ src/app/shop/shop.module.ts
@@ -4,9 +4,10 @@ import { ShopComponent } from './shop.component';
 import { ShopRoutingModule } from './shop-routing.module';
 import { ProductItemComponent } from './product-item/product-item.component';
 import { SharedModule } from '../shared/shared.module';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 @NgModule({
-  declarations: [ShopComponent, ProductItemComponent],
+  declarations: [ShopComponent, ProductItemComponent, ProductDetailsComponent],
   imports: [CommonModule, ShopRoutingModule, SharedModule],
   exports: [ShopComponent, ProductItemComponent],
 })
